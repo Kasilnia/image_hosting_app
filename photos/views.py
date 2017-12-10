6276132f09b48e855a2a91070ec0db4183060389
Kasilnia <kas.pieczynska@gmail.com>
2017-12-10 23:58:05 +0100

    Add photo creating view

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 from .models import Photo
 
@@ -7,3 +8,10 @@ class HomepageView(ListView):
     """HomepageView displays photos list and menu."""
     model = Photo
     template_name = 'photos/homepage.html'
+
+
+class PhotoCreateView(CreateView):
+    """PhotoCreateView creates new photo object."""
+    model = Photo
+    fields = ['title', 'image']
+    success_url = '/'  # TODO Redirect to details page
