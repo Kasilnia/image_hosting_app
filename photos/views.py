9bfc788bf3c8a567095a850b99b6bd9a027a9b75
Kasilnia <kas.pieczynska@gmail.com>
2017-12-11 00:22:35 +0100

    Delete photo object

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Photo
 
@@ -15,3 +16,8 @@ class PhotoCreateView(CreateView):
     model = Photo
     fields = ['title', 'image']
     success_url = '/'  # TODO Redirect to details page
+
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = reverse_lazy('photos:homepage')
